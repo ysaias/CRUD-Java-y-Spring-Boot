a35fb4349d1f46245d350c27e4161704823dce88
ysaias <ysaiasceron@gmail.com>
2024-05-24 10:40:22 -0400

    se agrega el frontend

--- src/main/java/com/microservicio/controller/CustomerController.java
+++ src/main/java/com/microservicio/controller/CustomerController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+    @RestController
 public class CustomerController {
 
     @Autowired
--- src/main/java/com/microservicio/entities/Customer.java
+++ src/main/java/com/microservicio/entities/Customer.java
@@ -21,4 +21,5 @@ public class Customer {
     private String address;
 
 
+
 }
